adf2bc5190b676aa5d6d6e34d64b71991fd06349
namiranda <49685356+namiranda@users.noreply.github.com>
2021-04-02 20:20:21 -0300

    Agrego referencia al owner en workspace y delete ws

--- src/models/workspace-model.js
+++ src/models/workspace-model.js
@@ -3,6 +3,10 @@ const noteSchema = require('./notes-model').schema;
 
 const workspaceSchema = new mongoose.Schema({
   name: String,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
   notes: [noteSchema],
 });
 
--- src/routes/workspace-routes.js
+++ src/routes/workspace-routes.js
@@ -6,7 +6,9 @@ router.get('/api/workspaces/:user_id', (req, res) => {
   User.findById(req.params.user_id)
     .populate('workspaces')
     .exec(function (err, foundUser) {
-      if (err) return handleError(err);
+      if (err) {
+        console.log(err);
+      }
       console.log(foundUser.workspaces);
       res.status(200).send(foundUser.workspaces);
     });
@@ -20,6 +22,7 @@ router.post('/api/workspaces/:user_id', (req, res) => {
     Workspace.create(
       {
         name: req.body.name,
+        owner: foundUser._id,
       },
       (err, newWorkspace) => {
         if (err) {
@@ -36,4 +39,25 @@ router.post('/api/workspaces/:user_id', (req, res) => {
   });
 });
 
+router.delete('/api/workspaces/:user_id/:workspace_id', (req, res) => {
+  Workspace.findById(req.params.workspace_id, (err, workspace) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (workspace.owner.equals(req.params.user_id)) {
+        //Verify that the user who want to delete de ws is the one who created
+        Workspace.deleteOne({ _id: workspace._id }, (err) => {
+          if (err) {
+            console.log(err);
+          }
+          res.status(200).send('The workspace was deleted successfully');
+        });
+      } else {
+        res.status(400).send('You are not allowed to perform this accion');
+      }
+    }
+  });
+});
+
 module.exports = router;
+//GIT: agrego owner ref y delete ws
