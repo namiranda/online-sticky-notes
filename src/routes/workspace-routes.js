2f929acf2bdff81e140c3daab9b8363c6c8b5f17
namiranda <49685356+namiranda@users.noreply.github.com>
2021-05-08 20:05:28 -0300

    modifico delete ws, ahora se borran ref en user y notas

--- src/routes/workspace-routes.js
+++ src/routes/workspace-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/user-model');
 const Workspace = require('../models/workspace-model');
+const Note = require('../models/notes-model');
 
 router.get('/api/workspaces/:user_id', (req, res) => {
   User.findById(req.params.user_id)
@@ -43,12 +44,29 @@ router.delete('/api/workspaces/:user_id/:workspace_id', (req, res) => {
     if (err) {
       console.log(err);
     } else {
+      //Verify that the user who want to delete de ws is the one who created
       if (workspace.owner.equals(req.params.user_id)) {
-        //Verify that the user who want to delete de ws is the one who created
+        //Deletes de notes in the workspace
+        Note.deleteMany()
+          .where('_id')
+          .in(workspace.notes)
+          .exec((err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
         Workspace.deleteOne({ _id: workspace._id }, (err) => {
           if (err) {
             console.log(err);
           }
+          User.findById(req.params.user_id).exec(function (err, user) {
+            if (err) {
+              console.log(err);
+            } else {
+              user.workspaces.pull({ _id: req.params.workspace_id });
+              user.save();
+            }
+          });
           res.status(200).send('The workspace was deleted successfully');
         });
       } else {
