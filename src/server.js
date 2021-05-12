69398da2e93ad3cc2dce2f8f4a872db41ed3530e
namiranda <naylenmiranda@gmail.com>
2021-05-12 20:42:16 -0300

    modifico cors config

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use(
 );
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.header('origin'));
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
